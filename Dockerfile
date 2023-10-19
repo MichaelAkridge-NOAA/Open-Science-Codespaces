78f5a8f76c53558796bef8a913aa6a0bb9a34997
Michael Akridge <michael.akridge@noaa.gov>
2023-10-19 13:22:35 -1000

    v0.5

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM alpine:3.15.4
+
+RUN apk update
+RUN apk add --no-cache git
+RUN apk add --no-cache docker-engine
+RUN apk add --no-cache docker-cli
+RUN apk add --no-cache docker-compose
+
+ADD ./app /app
+ADD ./setup_script.sh /app/setup_script.sh
+
+# Make the script executable during the build
+RUN chmod +x /app/setup_script.sh
+
+EXPOSE 1521 8181 8000 35729 3000 1313
---
+++ setup_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set up ORDS secrets
+echo 'CONN_STRING=user/password@hostname:port/service_name' > /app/ords_secrets/conn_string.txt
+
+# Navigate to the app directory
+cd /app
+
+# Start the Docker daemon
+dockerd &
+
+# Wait for the Docker daemon to initialize
+sleep 5
+
+# Bring up your services using docker-compose
+docker-compose up -d
+
+# Stop all running containers after setup (can be removed if you want the services to keep running)
+docker-compose stop
