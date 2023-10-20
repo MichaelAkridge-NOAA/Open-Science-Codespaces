45b1eb55a1a60d40e1da1dcb1cb0e385cfcfcb43
Michael Akridge <michael.akridge@noaa.gov>
2023-10-20 13:20:00 -1000

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,12 +6,12 @@ echo "Starting post-create setup..."
 apk update
 apk add --no-cache bash git docker-engine docker-cli docker-compose
 
+# Log in to Oracle Docker Registry
+echo $ORACLE_DOCKER_REGISTRY_PASSWORD | docker login container-registry.oracle.com --username $ORACLE_DOCKER_REGISTRY_USERNAME --password-stdin
+
 # Run the setup_script.sh
 echo "Executing setup_script.sh..."
 bash /app/setup_script.sh
-
-# Log in to Oracle Docker Registry
-echo $ORACLE_DOCKER_REGISTRY_PASSWORD | docker login container-registry.oracle.com --username $ORACLE_DOCKER_REGISTRY_USERNAME --password-stdin
 
 echo "Post-create setup completed."
 
