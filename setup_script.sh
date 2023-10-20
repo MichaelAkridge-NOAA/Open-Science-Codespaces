d4c1274e48d4bb658c6a90653e883697ecef3023
Michael Akridge <michael.akridge@noaa.gov>
2023-10-19 14:05:22 -1000

    v.7

--- setup_script.sh
+++ setup_script.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # Set up ORDS secrets
+mkdir -p /app/ords_secrets
+mkdir -p /app/ords_config
+mkdir -p /app/db-vol
+chmod -R 777 /app/db-vol
+chmod -R 777 /app/ords_config
+chmod -R 777 /app/ords_secrets
 echo 'CONN_STRING=user/password@hostname:port/service_name' > /app/ords_secrets/conn_string.txt
 
 # Navigate to the app directory
@@ -10,11 +16,10 @@ cd /app
 dockerd &
 
 # Wait for the Docker daemon to initialize
-sleep 5
+sleep 7
 
 # Bring up your services using docker-compose
-#docker-compose up -d
-docker-compose -f /app/docker-compose.yml up -d
+#docker-compose up
 
 # Stop all running containers after setup (can be removed if you want the services to keep running)
 #docker-compose stop
