90c634b95a998144acae5244026bbfd215a07a3d
Michael Akridge <michael.akridge@noaa.gov>
2023-10-25 15:53:27 -1000

    Update post_start.sh

--- .devcontainer/rstudio-test/workspace/post_start.sh
+++ .devcontainer/rstudio-test/workspace/post_start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo 'RStudio might be available at http://localhost:8787/'
-echo 'RStudio is starting. Try it at https://$CODESPACE_NAME-8787.app.github.dev'
+echo "RStudio is starting. Try it at https://$CODESPACE_NAME-8787.app.github.dev"
 sleep 10
