17ff5af4d294d52ef0a177fe4fdd11ed4b3c7df3
Michael Akridge <michael.akridge@noaa.gov>
2023-10-26 13:28:20 -1000

    t2

---
+++ .devcontainer/rstudio-test2/workspace/post_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Construct the message
+message="RStudio is starting. Try it at https://$CODESPACE_NAME-8787.app.github.dev"
+
+# Echo the message to the terminal
+echo "$message"
+
+# Append the message to README.md
+echo "$message" >> /home/rstudio/workspace/README.md
+
+sleep 10
+
