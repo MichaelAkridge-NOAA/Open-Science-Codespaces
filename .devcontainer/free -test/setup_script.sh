02eadecb4ef9c71e330ca807508e04e6622d0b7f
Michael Akridge <michael.akridge@noaa.gov>
2023-10-30 10:39:46 -1000

    test 3

---
+++ .devcontainer/free -test/setup_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p /app/ords_secrets
+mkdir -p /app/ords_config
+mkdir -p /app/db-vol
+chmod -R 777 /app/db-vol
+chmod -R 777 /app/ords_config
+chmod -R 777 /app/ords_secrets
+echo $CONN_STRING > /app/ords_secrets/conn_string.txt
+
+
+# Navigate to the app directory
+cd /app
+
+echo $ORACLE_DOCKER_REGISTRY_PASSWORD | docker login container-registry.oracle.com --username $ORACLE_DOCKER_REGISTRY_USERNAME --password-stdin
+
+docker compose up
