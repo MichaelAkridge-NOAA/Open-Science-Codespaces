bf93efbaac58d9891307864dd23467e32d851127
Michael Akridge <michael.akridge@noaa.gov>
2023-10-30 13:09:37 -1000

    test

--- app/setup_script.sh
+++ app/setup_script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-LOGFILE="/workspaces/Open-Science-Codespaces/app/test/app/docker_log.txt"
+LOGFILE="/workspaces/Open-Science-Codespaces/app/docker_log.txt"
 
 log() {
     echo "$1" | tee -a "$LOGFILE"
 }
 
 # Create the log file and set permissions
-touch "$LOGFILE"
 chmod 777 "$LOGFILE"
 # Initial logs or information
 cat <<EOL >> "$LOGFILE"
@@ -15,17 +14,17 @@ cat <<EOL >> "$LOGFILE"
 - Starting docker-compose services.
 ======================================================================
 echo $ORACLE_DOCKER_REGISTRY_PASSWORD | docker login container-registry.oracle.com --username $ORACLE_DOCKER_REGISTRY_USERNAME --password-stdin
-mkdir -p /workspaces/Open-Science-Codespaces/app/test/app/ords_secrets
-mkdir -p /workspaces/Open-Science-Codespaces/app/test/app/ords_config
-mkdir -p /workspaces/Open-Science-Codespaces/app/test/app/db-vol
-chmod -R 777 /workspaces/Open-Science-Codespaces/app/test/app/db-vol
-chmod -R 777 /workspaces/Open-Science-Codespaces/app/test/app/ords_config
-chmod -R 777 /workspaces/Open-Science-Codespaces/app/test/app/ords_secrets
-echo $CONN_STRING > /workspaces/Open-Science-Codespaces/app/test/app/ords_secrets/conn_string.txt
+mkdir -p /workspaces/Open-Science-Codespaces/app/ords_secrets
+mkdir -p /workspaces/Open-Science-Codespaces/app/ords_config
+mkdir -p /workspaces/Open-Science-Codespaces/app/db-vol
+chmod -R 777 /workspaces/Open-Science-Codespaces/app/db-vol
+chmod -R 777 /workspaces/Open-Science-Codespaces/app/ords_config
+chmod -R 777 /workspaces/Open-Science-Codespaces/app/ords_secrets
+echo $CONN_STRING > /workspaces/Open-Science-Codespaces/app/ords_secrets/conn_string.txt
 
 
 # Navigate to the app directory
-cd /workspaces/Open-Science-Codespaces/app/test
+cd /workspaces/Open-Science-Codespaces/app
 
 # Start the Docker daemon and log output
 dockerd &>> "$LOGFILE" &
@@ -35,15 +34,14 @@ log "Waiting for the Docker daemon to initialize..."
 sleep 7
 
 # Bring up your services using docker-compose and log output
-docker-compose up &>> "$LOGFILE"
+docker-compose up -d &>> "$LOGFILE"
 cat <<EOL >> "$LOGFILE"
 ======================================================================
                           Docker Compose Finished
 - Moving on to the next commands or steps.
 ======================================================================
 EOL
-# Stop all running containers after setup (can be removed if you want the services to keep running)
-#docker-compose stop &>> "$LOGFILE"
+
 
 cat <<EOL >> "$LOGFILE"
 ======================================================================
