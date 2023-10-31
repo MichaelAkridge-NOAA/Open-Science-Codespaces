36c233dc9d0ef19921606549abb0a1eb3e7332ae
Michael Akridge <michael.akridge@noaa.gov>
2023-10-30 14:41:54 -1000

    Update setup_script.sh

--- app/free-app/setup_script.sh
+++ app/free-app/setup_script.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+message="# Welcome to your Oracle DB & Apex Codespace!
+- This readme should open up at same time as the VS code container. 
+- If there are any persistent data changes, you might want to backup/export the data 
+- Install takes a bit to complete (~20mins)
+
+- Useful terminal commands
+### view containers
+docker ps
+### view container logs
+docker logs ords-apex
+
+# Access Oracle Apex
+## [Click to Open Apex](https://$CODESPACE_NAME-8181.app.github.dev)
+"
+# Append the message to README.md
+echo "$message$(cat /workspaces/Open-Science-Codespaces/app/free-app/README.md)" > /workspaces/Open-Science-Codespaces/app/free-app/temp_README.md
+mv /workspaces/Open-Science-Codespaces/app/free-app/temp_README.md /workspaces/Open-Science-Codespaces/README.md
+
 echo $ORACLE_DOCKER_REGISTRY_PASSWORD | docker login container-registry.oracle.com --username $ORACLE_DOCKER_REGISTRY_USERNAME --password-stdin
 mkdir -p /workspaces/Open-Science-Codespaces/app/free-app/ords_secrets
 mkdir -p /workspaces/Open-Science-Codespaces/app/free-app/ords_config
