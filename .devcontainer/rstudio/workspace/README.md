49e017b4026a763cdbb55285761d5445696ad73f
Michael Akridge <michael.akridge@noaa.gov>
2023-10-31 13:28:53 -1000

    cleanup

--- .devcontainer/rstudio/workspace/README.md
+++ .devcontainer/rstudio/workspace/README.md
@@ -1,11 +1,6 @@
 # Welcome to your RStudio Codespace!
 - This readme should open up at same time as the VS code container. 
-- If there are any persistent data changes, you might want to back up the 
+- If there are any persistent data changes, you might want to backup/export the data 
 
 # Access RStudio
 Your application is available at this URL:
-
-
-
-
-
--- .devcontainer/rstudio/workspace/post_start.sh
+++ .devcontainer/rstudio/workspace/post_start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 # Construct the message
-message="# Welcome to your RStudio Codespace!
-- This readme should open up at same time as the VS code container. 
-- If there are any persistent data changes, you might want to backup/export the data 
-
-# Access RStudio
-## [Click to Open RStudio](https://$CODESPACE_NAME-8787.app.github.dev)
+message="## [Click to Open RStudio](https://$CODESPACE_NAME-8787.app.github.dev)
 "
 # Echo the message to the terminal
 echo "$message"
