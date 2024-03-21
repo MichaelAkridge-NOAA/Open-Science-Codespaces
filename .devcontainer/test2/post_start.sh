5c43c95390425099a3d679a9257665d097be55ef
Michael Akridge <michael.akridge@noaa.gov>
2024-03-21 13:56:42 -1000

    Update post_start.sh

--- .devcontainer/test2/post_start.sh
+++ .devcontainer/test2/post_start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Clone the GitHub repository
-git clone https://github.com/coastwatch-training/CoastWatch-Tutorials.git ./CoastWatch-Tutorials
+git clone https://github.com/coastwatch-training/CoastWatch-Tutorials.git /workspaces/Open-Science-Codespaces/CoastWatch-Tutorials
 
 # Construct the message
 message="## [Click to Open RStudio](https://$CODESPACE_NAME-8787.app.github.dev)
-## [Click to Open JupyterLab(https://$CODESPACE_NAME-8888.app.github.dev)
+## [Click to Open Jupyte(https://$CODESPACE_NAME-8888.app.github.dev?editor=jupyter)
 "
 # Echo the message to the terminal
 echo "$message"
