721c8149f648f8866993567662ac085787567abd
Michael Akridge <michael.akridge@noaa.gov>
2024-03-21 14:08:51 -1000

    test

---
+++ .devcontainer/python-and-r/post_start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Clone the GitHub repository
+git clone https://github.com/coastwatch-training/CoastWatch-Tutorials.git /workspaces/Open-Science-Codespaces/CoastWatch-Tutorials
+
+# Construct the message
+message="
+RStudio editor: https://$CODESPACE_NAME-8787.app.github.dev
+
+JupyterLab editor: https://$CODESPACE_NAME.github.dev?editor=jupyter
+
+"
+# Echo the message to the terminal
+echo "$message"
+
+# Append the message to README.md
+echo "$message" >> ./README.md
+
+echo "
+Open the RStudio editor here: https://$CODESPACE_NAME-8787.app.github.dev
+   - (This may take a few seconds to load, retry if necessary)
+
+Open the JupyterLab editor here: https://$CODESPACE_NAME.github.dev?editor=jupyter
+   - (This may take a few seconds to load, retry if necessary)
+
+"
+sleep 10 
