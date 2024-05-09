2b545ac350e2af60749eee3acfe5eab4325de99f
Michael Akridge - NOAA <65312823+MichaelAkridge-NOAA@users.noreply.github.com>
2024-05-09 11:05:37 -1000

    Update post_start.sh

--- .devcontainer/rstudio-tidyverse/workspace/post_start.sh
+++ .devcontainer/rstudio-tidyverse/workspace/post_start.sh
@@ -3,12 +3,13 @@
 # Construct the message
 message="## [Click to Open RStudio](https://$CODESPACE_NAME-8787.app.github.dev)
 "
+
 # Echo the message to the terminal
 echo "$message"
 
-# Append the message to README.md
-echo "$message" >> /home/rstudio/workspace/README.md
+# Check if the message is already in README.md and append if it's not
+if ! grep -Fq "$message" /home/rstudio/workspace/README.md; then
+  echo "$message" >> /home/rstudio/workspace/README.md
+fi
 
 sleep 10
-
-
