adc19c7022c5152b1f554d62932fb7ee744f26eb
Michael Akridge - NOAA <65312823+MichaelAkridge-NOAA@users.noreply.github.com>
2024-05-09 11:06:47 -1000

    Update post_start.sh

--- .devcontainer/python-streamlit-app-example/post_start.sh
+++ .devcontainer/python-streamlit-app-example/post_start.sh
@@ -3,12 +3,13 @@
 # Construct the message
 message="## [Click to Open Streamlit App Example | NCRMP Data Explorer & Viz Tool](https://$CODESPACE_NAME-8501.app.github.dev)
 "
+
 # Echo the message to the terminal
 echo "$message"
 
-# Append the message to README.md
-echo "$message" >> /app/README.md
+# Check if the message is already in README.md and append if it's not
+if ! grep -Fq "$message"  /app/README.md; then
+  echo "$message" >>  /app/README.md
+fi
 
 sleep 10
-
-
