8ccbc686213094a2b0ea8c7004f6e11d1d6145bc
Michael Akridge - NOAA <65312823+MichaelAkridge-NOAA@users.noreply.github.com>
2024-05-09 11:05:48 -1000

    Update post_start.sh

--- .devcontainer/rstudio-shiny/workspace/post_start.sh
+++ .devcontainer/rstudio-shiny/workspace/post_start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Construct the message
-message="## [Click to Open Shiny Server](https://$CODESPACE_NAME-3838.app.github.dev)
+message="## [Click to Open RStudio](https://$CODESPACE_NAME-8787.app.github.dev)
 "
+
 # Echo the message to the terminal
 echo "$message"
 
-# Append the message to README.md
-echo "$message" >> /home/rstudio/workspace/README.md
+# Check if the message is already in README.md and append if it's not
+if ! grep -Fq "$message" /home/rstudio/workspace/README.md; then
+  echo "$message" >> /home/rstudio/workspace/README.md
+fi
 
 sleep 10
-
-
