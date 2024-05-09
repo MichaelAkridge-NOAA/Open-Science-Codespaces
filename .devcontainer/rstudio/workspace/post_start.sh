20980c95ae6298d826fc37c749626245703a3512
Michael Akridge - NOAA <65312823+MichaelAkridge-NOAA@users.noreply.github.com>
2024-05-09 11:09:54 -1000

    Update post_start.sh

--- .devcontainer/rstudio/workspace/post_start.sh
+++ .devcontainer/rstudio/workspace/post_start.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 # Construct the message
-message="## [Click to Open RStudio](https://$CODESPACE_NAME-8787.app.github.dev)
-"
+message="## [Click to Open RStudio](https://$CODESPACE_NAME-8787.app.github.dev)"
+echo "Debug: Constructed message."
 
 # Echo the message to the terminal
 echo "$message"
 
 # Check if the message is already in README.md and append if it's not
-if ! grep -Fq "$message" /home/rstudio/workspace/README.md; then
-  echo "$message" >> /home/rstudio/workspace/README.md
+echo "Debug: Checking if message is in README.md."
+if ! grep -Fqx "$message" /home/rstudio/workspace/README.md; then
+    echo "Debug: Message not found in README.md, appending now."
+    echo "$message" >> /home/rstudio/workspace/README.md
+else
+    echo "Debug: Message already in README.md, not appending."
 fi
 
 sleep 10
